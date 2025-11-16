2c682b438a84ffbecc91bdded64342bede98eb9d
PavanKumar-sudo <pavanvinjamuri212@gmail.com>
2025-11-16 12:23:43 -0700

    first commit

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,7 +2,7 @@ export const LINKS = {
   githubProfile: "https://github.com/PavanKumar-sudo",
   linkedinProfile: "https://linkedin.com/in/pavan-vinjamuri",
   quickAiGithub: "https://github.com/PavanKumar-sudo/QuickAI",
-  quickAiLive: "https://quick-ai-cl.vercel.app/",
+  quickAiLive: "https://quick-ai-client-eight.vercel.app/",
   githubRepositories: "https://github.com/PavanKumar-sudo?tab=repositories",
 } as const;
 
